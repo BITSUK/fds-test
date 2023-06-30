85aa18e9c3cf5341fe698e9322269a0edfc0d489
BITSUK <2022cfse006@wilp.bits-pilani.ac.in>
2023-06-30 08:31:16 +0530

    improved test cases

--- fds-selenium-test.py
+++ fds-selenium-test.py
@@ -4,8 +4,8 @@ from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome()
 
-# driver.get("http://localhost:3000/")
-driver.get("http://fdsapp.s3-website.ap-south-1.amazonaws.com")
+driver.get("http://localhost:3000/")
+#driver.get("http://fdsapp.s3-website.ap-south-1.amazonaws.com")
 
 title = driver.title
 assert title == "FDS Web App"
@@ -23,18 +23,43 @@ assert value == ""
 print ('PASSED   Test 01: Check if website site is up.')
 driver.implicitly_wait(1)
 
-submit_button = driver.find_element(by=By.ID, value="srchBtn")
+station_radio = driver.find_element(by=By.ID, value="radioStation")
+station_radio.click()
 
-text_box.send_keys("Selenium")
-print ('PASSED   Test 02: Able to interact.')
 driver.implicitly_wait(3)
 
-# submit_button.click()
 
-# message = driver.find_element(by=By.ID, value="message")
-# value = message.text
-# assert value == "Received!"
-print ('SKIPPED  Test 02: Able to interact with backend.')
+returned_elements = driver.find_elements(By.CLASS_NAME,"table-row")
+found = "false"
+for i in returned_elements:
+    if i.text.__contains__("Ratlam"):
+        found = "true"
+
+if found == "true":
+    print ('PASSED   Test 02: Check if backend returning value.')
+else:
+    print ('FAILED   Test 02: Check if backend returning value.')
+
+
+submit_button = driver.find_element(by=By.ID, value="srchBtn")
+text_box.send_keys("KOTA")
+print ('PASSED   Test 03: Able to interact with website.')
+
+driver.implicitly_wait(3)
+
+submit_button.click()
+
+returned_elements = driver.find_elements(By.CLASS_NAME,"table-row")
+found = "false"
+for i in returned_elements:
+    if i.text.__contains__("Ratlam"):
+        found = "true"
+
+if found == "false":
+    print ('PASSED   Test 04: Check if site is functional.')
+else:
+    print ('FAILED   Test 04: Check if site is functional.')
+
 driver.implicitly_wait(2)
 
 print("")
